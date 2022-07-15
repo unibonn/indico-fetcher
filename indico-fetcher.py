53c1f3d59b0169cf8310f19de0216e8a122edf37
Oliver Freyermuth <freyermuth@physik.uni-bonn.de>
2022-07-15 04:14:46 +0200

    Consistify indentation.

--- indico-fetcher.py
+++ indico-fetcher.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-import requests
-import urllib.request
 import json
 import os
 import time
+import urllib.request
+import requests
 import dateutil.parser
 
 try:
@@ -34,57 +34,57 @@ if __name__ == '__main__':
     request_path = build_indico_request(PATH, PARAMS)
     req = exec_request(INDICO_INSTANCE, request_path)
     try:
-      jresp = json.loads(req.content)
+        jresp = json.loads(req.content)
     except:
-      print("Error parsing JSON response!")
-      os.exit(1)
+        print("Error parsing JSON response!")
+        os.exit(1)
 
     for contrib in jresp['results'][0]['contributions']:
-      if 'startDate' not in contrib:
-        continue
-      if contrib['startDate'] is None:
-        continue
+        if 'startDate' not in contrib:
+            continue
+        if contrib['startDate'] is None:
+            continue
 
-      date = contrib['startDate']['date']
-      fulldir = date
-      try:
-        os.mkdir(fulldir)
-      except FileExistsError:
-        pass
+        date = contrib['startDate']['date']
+        fulldir = date
+        try:
+            os.mkdir(fulldir)
+        except FileExistsError:
+            pass
 
-      try:
-        room = contrib['room'].split().pop(-1)
-      except:
-        room = contrib['room']
+        try:
+            room = contrib['room'].split().pop(-1)
+        except:
+            room = contrib['room']
+  
+        if len(room) > 0:
+            fulldir += "/" + room
+        try:
+            os.mkdir(fulldir)
+        except FileExistsError:
+            pass
 
-      if len(room) > 0:
-        fulldir += "/" + room
-      try:
-        os.mkdir(fulldir)
-      except FileExistsError:
-        pass
+        slot = contrib['startDate']['time'] + "_" + contrib['endDate']['time']
+        fulldir += "/" + slot
+        try:
+            os.mkdir(fulldir)
+        except FileExistsError:
+            pass
 
-      slot = contrib['startDate']['time'] + "_" + contrib['endDate']['time']
-      fulldir += "/" + slot
-      try:
-        os.mkdir(fulldir)
-      except FileExistsError:
-        pass
+        folders = contrib['folders']
 
-      folders = contrib['folders']
+        for folder in folders:
+            for attachment in folder['attachments']:
+                mod = attachment['modified_dt']
+                mtime = dateutil.parser.parse(mod)
+                mtimets = int(time.mktime(mtime.timetuple()))
+                full_filename = fulldir + "/" + str(mtimets) + "_" + attachment['filename']
 
-      for folder in folders:
-        for attachment in folder['attachments']:
-          mod = attachment['modified_dt']
-          mtime = dateutil.parser.parse(mod)
-          mtimets = int(time.mktime(mtime.timetuple()))
-          full_filename = fulldir + "/" + str(mtimets) + "_" + attachment['filename']
-
-          if not os.path.isfile(full_filename):
-            print('Downloading: ' + full_filename)
-            try:
-              urllib.request.urlretrieve(attachment['download_url'], full_filename)
-            except Exception as exc:
-              print(exc)
-              print('  Encountered unknown error. Continuing.')
-
+                if not os.path.isfile(full_filename):
+                    print('Downloading: ' + full_filename)
+                    try:
+                        urllib.request.urlretrieve(attachment['download_url'], full_filename)
+                    except Exception as exc:
+                        print(exc)
+                        print('  Encountered unknown error. Continuing.')
+  
